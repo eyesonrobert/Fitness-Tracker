bcc8d373ef13ef6501ad16929a00a539c3a5e39d
Robert Franklin <eyesonrobert@gmail.com>
2019-09-28 10:35:12 -0700

    production environment initialized

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB0D52mM1S6oDAWyanvUq-DqqYnRf136PE',
+    authDomain: 'eyesonrobert-fitness-tracker.firebaseapp.com',
+    databaseURL: 'https://eyesonrobert-fitness-tracker.firebaseio.com',
+    projectId: 'eyesonrobert-fitness-tracker',
+    storageBucket: 'eyesonrobert-fitness-tracker.appspot.com',
+    messagingSenderId: '743085775851',
+    appId: '1:743085775851:web:f2cc489c53b217b21f97ae'
+  }
 };
